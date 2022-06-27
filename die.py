6b01181c0e1f9923eab7366d5e5b628aafbd3fa0
PilarczykM <marcinpilarczyk@me.com>
2022-06-27 17:21:30 +0200

    Reorganize die class

--- die.py
+++ die.py
@@ -2,23 +2,21 @@ import random
 
 
 class Die(object):
-
-    def __init__(self, sides=6):
+    def __init__(self, sides=6, face: int = None):
         self.sides = sides
-        # Make __face private to prevent cheating
-        self.__face = None
+        if face is not None:
+            self.__face = face
+        else:
+            self.roll()
 
     def roll(self):
         self.__face = random.randint(1, self.sides)
 
-    def clear(self):
-        self.__face = None
-
     def get_face(self):
         return self.__face
 
+    def set_face(self, face):
+        self.__face = face
+
     def __str__(self):
-        if self.__face:
-            return "Value: " + str(self.__face)
-        else:
-            return "Die has not been thrown"
+        return str(self.__face)
