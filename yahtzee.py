da3ada15ef5584a4144bc85308ab59febf2ddf2e
PilarczykM <marcinpilarczyk@me.com>
2022-06-27 18:49:33 +0200

    Update yahtzee.py
    Remove old code

--- yahtzee.py
+++ yahtzee.py
@@ -1,34 +1,124 @@
+import os
 import re
 
+from hand import Hand
+from scoreboard import ScoreBoard
+from rules.yahtzee_role import (Aces, Chance, FibonYahtzee, Fives, FourOfAKind,
+                                Fours, FullHouse, LargeStraight, Sixes,
+                                SmallStraight, ThreeOfAKind, Threes, Twos, Yahtzee)
 
-def re_roll(self):
-    rolls = 0
-    while rolls < 2:
-        try:
-            re_roll = input("\nChoose which dice to re-roll "
-                            "(comma-separated or 'all'), or 0 to continue: ")
 
-            if re_roll.lower() == "all":
-                re_roll = list(range(1, 6))
-            else:
-                # Perform some clean-up of input
-                re_roll = re_roll.replace(" ", "")  # Remove spaces
-                re_roll = re.sub('[^\d,]', '', re_roll)  # Remove non-numerals
-                re_roll = re_roll.split(",")  # Turn string into list
-                re_roll = list(map(int, re_roll))  # Turn strings in list to int
-        except ValueError:
-            print("You entered something other than a number.")
-            print("Please try again")
-            continue
+class YahtzeeGame:
+    def __init__(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("""
+            YAHTZEE
+            Welcome to the game. To begin, simply press [Enter]
+            and follow the instructions on the screen.
+            To exit, press [Ctrl+C]
+            """)
 
-        if [x for x in re_roll if x > self.dice]:
-            print("You only have 5 dice!")
-            continue
+        # Begin by instantiating the hand and scoreboard
+        self.hand = Hand()
+        self.scoreboard = ScoreBoard()
 
-        if not re_roll or 0 in re_roll:
-            break
-        else:
-            for i in re_roll:
-                self.hand[i - 1].roll()
-            self.show_hand()
+        # Register the rules for Yahtzee
+        self.scoreboard.register_rules([
+            Aces(),
+            Twos(),
+            Threes(),
+            Fours(),
+            Fives(),
+            Sixes(),
+            ThreeOfAKind(),
+            FourOfAKind(),
+            FullHouse(),
+            SmallStraight(),
+            LargeStraight(),
+            Yahtzee(),
+            FibonYahtzee(),
+            Chance(),
+        ])
+
+    def choose_dice_reroll(self):
+        while True:
+            try:
+                reroll = input("\nChoose which dice to re-roll "
+                               "(comma-separated or 'all'), or 0 to continue: ")
+
+                if reroll.lower() == "all":
+                    return self.hand.all_dice()
+                else:
+                    # Perform some clean-up of input
+                    reroll = reroll.replace(" ", "")  # Remove spaces
+                    reroll = re.sub('[^0-9,]', '', reroll)  # Remove non-numerals
+                    reroll = reroll.split(",")  # Turn string into list
+                    reroll = list(map(int, reroll))  # Turn strings in list to int
+
+                if not reroll or 0 in reroll:
+                    return []
+                else:
+                    return reroll
+
+            except ValueError:
+                print("You entered something other than a number.")
+                print("Please try again")
+            except IndexError as i:
+                print(i)
+
+    def select_scoring(self):
+        self.show_scoreboard_points(self.hand)
+        while True:
+            scoreboard_row = input("Choose which scoring to use: ")
+            try:
+                scoreboard_row_int = int(re.sub('[^0-9,]', '', scoreboard_row))
+                if scoreboard_row_int < 1 or scoreboard_row_int > self.scoreboard.rule_count():
+                    print("Please select an existing scoring rule.")
+                else:
+                    return self.scoreboard.get_rule(scoreboard_row_int - 1)
+            except ValueError:
+                print("You entered something other than a number. Please try again.")
+
+    def show_scoreboard_points(self, hand: Hand = None):
+        print("\nSCOREBOARD")
+        print("===================================")
+        print(self.scoreboard.create_points_overview(hand))
+        print("===================================")
+
+    def do_turn(self):
+        rolls = 0
+        selected_dice = self.hand.all_dice()
+        while True:
+            # roll the dice
+            print("\nRolling dice...")
+            self.hand.roll(selected_dice)
+            print(self.hand)
             rolls += 1
+
+            # if we reached the maximum number of rolls, we're done
+            if rolls >= 3:
+                break
+
+            # choose which dice to reroll, break if empty
+            selected_dice = self.choose_dice_reroll()
+            if len(selected_dice) == 0:
+                break
+
+        rule = self.select_scoring()
+
+        points = self.scoreboard.assign_points(rule, self.hand)
+        print(f"Adding {points} points to {rule.name()}")
+        self.show_scoreboard_points()
+
+        input("\nPress any key to continue")
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    def play(self):
+
+        # We keep going until the scoreboard is full
+        for _ in range(self.scoreboard.rule_count()):
+            self.do_turn()
+
+        print("\nCongratulations! You finished the game!\n")
+        self.show_scoreboard_points()
+        print(f"Total points: {self.scoreboard.total_points()}")
